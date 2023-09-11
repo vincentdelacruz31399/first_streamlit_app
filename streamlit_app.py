80ca7e14364eef51a157a02dd40dcc79a21974c4
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-11 09:37:46 +0800

    Update streamlit_app.py
    added emojis again and title

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import streamlit
 streamlit.title('My Mom's New Healthy Diner')
 #Body of the streamlit
 streamlit.header('Breakfast Menu')
-streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit.text(' Kale, Spinach & Rocket Smoothie')
-streamlit.text(' Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
