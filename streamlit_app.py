a8870523933328bd7406ff63ae87719d139f766e
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-11 10:25:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #to use the fruits in our fruits_selected list to pull rows from the full dataset(and assigned that data to a variable 
 #called fruits_to_show).Finally, we'll ask the app to use the data in fruits_to_show in the dataframe it displays on the page
 fruits_selected = streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(fruit_selected)
+streamlit.dataframe(fruits_selected)
 #use myfruitlist dataframe then put inside [fruit_selected]
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #pull data into the dataframe using the variable my_fruit_list use now the fruits_to_show dataframe for selected 
