56f73905845287eaf113ede0438fb7bc9b6114c7
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-11 09:32:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 #Body of the streamlit
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
+streamlit.text(' Kale, Spinach & Rocket Smoothie')
+streamlit.text(' Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
