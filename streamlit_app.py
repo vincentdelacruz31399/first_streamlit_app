c0f33cba70a67a1d0585bbb97eb89e7ff1638c4a
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-11 09:34:03 +0800

    Update streamlit_app.py
    Added new Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text(' Omega 3 & Blueberry Oatmeal')
 streamlit.text(' Kale, Spinach & Rocket Smoothie')
 streamlit.text(' Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
