9819e829ae5dbc8a79f4e4614070c383f34c2122
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-11 09:31:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 #"Commit" means "SAVE" every time "Commit" we can put note on what is the changes
 import streamlit
+#title of the streamlit
 streamlit.title('My Parents New Healthy Dinner')
-
+#Body of the streamlit
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
