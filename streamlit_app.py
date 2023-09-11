e39b6bf026a09bb8da42b679b9d57f3f0662ae45
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-11 09:25:15 +0800

    Update streamlit_app.py
    Added Header 
    Added first text or body of streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 #"Commit" means "SAVE" every time "Commit" we can put note on what is the changes
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
