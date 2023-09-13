0fffb1c91c2a021f883ad1f3ec7f858cf9ca8233
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-13 14:01:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,7 +109,7 @@ try:
                 back_from_function = insert_row_snowflake(add_my_fruit) #call new variable which value is function insert_row_function with add_my_fruit inside which is input button where user type what fruit they want to add
                 my_cnx.close() # close all connetion at the end of button script
                 streamlit.text(back_from_function)
-                streamlit.clear(add_my_fruit)
+                add_my_fruit.clear()
         
 except URLError as e:
     streamlit.error()
