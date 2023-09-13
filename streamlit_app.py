42343cf34127c1a15e4612e2fd33a626518ce42b
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-13 13:52:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,8 +107,8 @@ try:
                 streamlit.button('Add a Fruit to the List')
                 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) #if the button is click then connect the variable to the secret account
                 back_from_function = insert_row_snowflake(add_my_fruit) #call new variable which value is function insert_row_function with add_my_fruit inside which is input button where user type what fruit they want to add
-                my_cnx.clear(add_my_fruit)
                 my_cnx.close() # close all connetion at the end of button script
                 streamlit.text(back_from_function)
+                my_cnx.clear(add_my_fruit)
 except URLError as e:
     streamlit.error()
