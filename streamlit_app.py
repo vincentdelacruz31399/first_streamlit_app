eaa1f1c089eb9919cfaf227d6bf8046fbdad0049
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-13 14:30:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,9 +99,8 @@ def insert_row_snowflake(new_fruit): #create new function to add the fruit name 
                 return "Thanks for adding " +  new_fruit     #return a word + the new value that inserted in snowflake
                 
                 
-
 add_my_fruit = streamlit.text_input('What fruit would you like to add?') #text , example output the 'KIWI' is the example output
-  if    streamlit.button('Add a Fruit to the List')
+if    streamlit.button('Add a Fruit to the List'):
          my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) #if the button is click then connect the variable to the secret account
          back_from_function = insert_row_snowflake(add_my_fruit) #call new variable which value is function insert_row_function with add_my_fruit inside which is input button where user type what fruit they want to add
          my_cnx.close()  #close all connetion at the end of button script
