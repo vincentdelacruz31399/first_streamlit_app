35fd9d0561140e9d78012a02691ba54c2fdd884c
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-13 13:29:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,8 @@ try:
         add_my_fruit = streamlit.text_input('What fruit would you like to add?') #text , example output the 'KIWI' is the example output
         if not add_my_fruit:
          streamlit.error("Please select a fruit to get information.") #error message if not part of fruit list
-        else streamlit.button('Add a Fruit to the List'):
+        else:
+                streamlit.button('Add a Fruit to the List'):
                 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) #if the button is click then connect the variable to the secret account
                 back_from_function = insert_row_snowflake(add_my_fruit) #call new variable which value is function insert_row_function with add_my_fruit inside which is input button where user type what fruit they want to add
                 my_cnx.close() # close all connetion at the end of button script
