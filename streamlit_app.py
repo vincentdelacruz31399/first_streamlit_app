8496bdb4d5e5a1d671087047b32c80c819eb13b2
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-09-13 13:06:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,7 @@ if streamlit.button('Get Fruit Load List'):
 def insert_row_snowflake(new_fruit): #create new function to add the fruit name submissions to the snowflake table
         with my_cnx.cursor() as my_cur:
                 my_cur.execute("insert into fruit_load_list values ('from streamlit')") #function that will insert value into the snowflake table
-                return "Thanks for adding" +  new_fruit     #return a word + the new value that inserted in snowflake
+                return "Thanks for adding " +  new_fruit     #return a word + the new value that inserted in snowflake
                 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?') #text , example output the 'KIWI' is the example output
 if streamlit.button('Add a Fruit to the List'):
